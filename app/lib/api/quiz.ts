1e61272c735b2ff053e615769cd9b864b26581c1
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2025-12-07 17:16:19 +0100

    quiz service api

--- app/lib/api/quiz.ts
+++ app/lib/api/quiz.ts
@@ -1,16 +1,118 @@
 import { supabase } from "../supabaseClient";
 
 //get quiz
-export const getQuiz = async ()=>{
-    const {data, error}= await supabase
-   .from('quizzes')
-   .select("*")
-   
-   if(error) {
-   console.log("Failed to get quiz:", error)
-   }
-   return {data, error}
+export const getQuiz = async () => {
+  const { data, error } = await supabase
+    .from("quizzes")
+    .select("*")
+    .order("created_at", { ascending: false });
 
+  if (error) {
+    console.log("Failed to get quiz:", error);
+  }
+  return { data, error };
+};
+
+//get quiz by id
+export async function getQuizById(quizId: number) {
+  const { data, error } = await supabase
+    .from("quizzes")
+    .select(
+      `
+      *,
+      quiz_questions (
+        *,
+        quiz_options (*)
+      )
+    `
+    )
+    .eq("id", quizId)
+    .single();
+
+  return { data, error };
 }
 
 //Create quiz
+export const createQuiz = async (quiz: {
+  title: string;
+  description: string;
+  duration_minutes: Date;
+  courseId: string;
+}) => {
+  const { data, error } = await supabase
+    .from("quiz")
+    .insert([
+      {
+        title: quiz.title,
+        description: quiz.description,
+        duration: quiz.duration_minutes,
+        course_id: quiz.courseId,
+      },
+    ])
+    .select()
+    .single();
+
+  return { data, error };
+};
+
+//create question
+export async function createQuestion(
+  quizId: number,
+  question: string,
+  position: number
+) {
+  const { data, error } = await supabase
+    .from("quiz_questions")
+    .insert([{ quiz_id: quizId, question, position }])
+    .select()
+    .single();
+
+  return { data, error };
+}
+
+//create option
+export async function createOption(
+  questionId: number,
+  option_text: string,
+  is_correct: boolean,
+  position: number
+) {
+  const { data, error } = await supabase
+    .from("quiz_options")
+    .insert([{ question_id: questionId, option_text, is_correct, position }])
+    .select()
+    .single();
+
+  return { data, error };
+}
+
+//submit quiz
+export async function submitQuiz(
+  quizId: number,
+  student_id: number,
+  answers: any[]
+) {
+  // 1️⃣ Create submission record
+  const { data: submission, error: submissionError } = await supabase
+    .from("quiz_submissions")
+    .insert([{ quiz_id: quizId, student_id }])
+    .select()
+    .single();
+
+  if (submissionError) return { error: submissionError };
+
+  const submission_id = submission.id;
+
+  // 2️⃣ Insert answers
+  const formattedAnswers = answers.map((a) => ({
+    submission_id,
+    question_id: a.question_id,
+    selected_option_id: a.selected_option_id,
+  }));
+
+  const { error: answersError } = await supabase
+    .from("quiz_answers")
+    .insert(formattedAnswers);
+
+  return { data: submission, error: answersError };
+}
